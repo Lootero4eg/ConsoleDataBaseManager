06dcdf6a5a448e5788ccdc1cc93472397e21ecaf
Lootero4eg <_papacrash_@mail.ru>
2017-01-24 11:15:06 +0500

    1
    Консольный менеджер баз данных

---
+++ ConsoleDataBaseManager/cdbm_main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using cdbm.Enums;
+
+namespace cdbm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cmd = "";
+            while (cmd != "quit")
+            {
+                Console.Write("cdbm> ");
+                cmd = Console.ReadLine();
+
+                if (cmd == "help")
+                {
+                    var res = Enum.GetNames(typeof(BaseCommands));
+                    string descr = "";
+                    for (int i = 0; i < res.Length; i++)
+                        descr += res[i] + " ";
+                    Console.WriteLine(descr);
+                }
+            }
+        }
+    }
+}
